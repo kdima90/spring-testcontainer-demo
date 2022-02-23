2144d31ae59947fb1cf208b1810c4dbe05672fb4
kdima90 <dimon19901990@mail.ru>
2022-02-23 21:22:10 +0100

    add comment to flyways V3 script

--- src/main/resources/db/migration/V3__insert_dummys.sql
+++ src/main/resources/db/migration/V3__insert_dummys.sql
@@ -1,2 +1,3 @@
+-- add some example users via flyway script (do not want use spring boot commandlinerunner :))
 insert into person.person(id, name) values (1, 'John Doe');
 insert into person.person(id, name) values (2, 'Jane Doe');
